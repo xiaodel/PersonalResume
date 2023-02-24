145d05cfc1c830b586a67052e88aec22611eb584
zhangjun <1580043700@qq.com>
2023-02-24 17:34:07 +0800

    first commit

---
+++ assets/AboutView-689d5a75.js
@@ -0,0 +1,1 @@
+import{_ as e}from"./plugin-vueexport-helper-c27b6911.js";import{o as t,c as o,f as _}from"./index-b2104056.js";const s={},c={class:"about"},n=_("h1",null,"关于我们",-1),a=[n];function r(i,d){return t(),o("div",c,a)}const f=e(s,[["render",r]]);export{f as default};
