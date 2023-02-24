1d915d7ba7bef66ba16cb5cfc4e7e41acbbe4f35
zhangjun <1580043700@qq.com>
2023-02-24 15:17:24 +0800

    首页样式优化

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,26 +1,35 @@
 <script setup>
 import templates from "@/components/template/index";
 import data from "@/data/data.json";
-import {reactive} from "vue";
 
-const width = 500;
-const styleObject = reactive({width: width + 'px', height: (297 / 210 * width) + 'px'});
 </script>
 
 <template>
   <nav class="nav">
     <div class="item" v-for="(value,key) in templates" :key="key">
-      <RouterLink :to="`/details/${key}`" :style="styleObject">
+      <RouterLink :to="`/details/${key}`" class="link">
         <component :is="value" :data="data"></component>
       </RouterLink>
     </div>
   </nav>
 </template>
 
-<style>
+<style lang="scss">
 
-.nav .item {
-  width: 300px;
+.nav {
+  .item {
+    width: 600px;
+    height: 800px;
+    overflow-y: auto;
+    border: 1px solid #2c3e50;
+    transform: scale(0.5);
+
+    &::-webkit-scrollbar {
+      width: 8px;
+      height: 10px;
+    }
+
+  }
 }
 
 /*@media (min-width: 1024px) {*/
