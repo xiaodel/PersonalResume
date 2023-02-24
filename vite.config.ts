4bc3ba242534459d5bec9ead1abaaa8fb7c45b31
zhangjun <1580043700@qq.com>
2023-02-24 17:24:14 +0800

    打包配置设置

--- vite.config.ts
+++ vite.config.ts
@@ -6,13 +6,32 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 // @ts-ignore
 import {viteCommonjs} from '@originjs/vite-plugin-commonjs'
 
+const INVALID_CHAR_REGEX = /[\x00-\x1F\x7F<>*#"{}|^[\]`;?:&=+$,]/g;
+const DRIVE_LETTER_REGEX = /^[a-z]:/i;
+
 // https://vitejs.dev/config/
 export default defineConfig({
+    base: '/PersonalResume/',
     plugins: [vue(), vueJsx(), viteCommonjs()],
     resolve: {
         alias: {
             '@': fileURLToPath(new URL('./src', import.meta.url))
         }
     },
-    base: '/PersonalResume/'
+    build: {
+        rollupOptions: {
+            output: {
+                // https://github.com/rollup/rollup/blob/master/src/utils/sanitizeFileName.ts
+                sanitizeFileName(name) {
+                    const match = DRIVE_LETTER_REGEX.exec(name);
+                    const driveLetter = match ? match[0] : '';
+                    // substr 是被淘汰語法，因此要改 slice
+                    return (
+                        driveLetter +
+                        name.slice(driveLetter.length).replace(INVALID_CHAR_REGEX, "")
+                    );
+                },
+            },
+        },
+    },
 })
