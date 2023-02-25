89cd24bf64b3bb053499bba6f1dfbd709c4aaa84
zhangjun <1580043700@qq.com>
2023-02-25 16:06:16 +0800

    首页样式优化

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,45 +5,68 @@ import data from "@/data/data.json";
 </script>
 
 <template>
-  <div class="nav">
-    <div class="item" v-for="(value,key) in templates" :key="key">
-      <RouterLink :to="`/details/${key}`" class="link">
-        <component :is="value" :data="data"></component>
-      </RouterLink>
-    </div>
+  <div class="pages">
+    <el-row :gutter="24">
+      <el-col :xs="24" :sm="24" :md="24" :lg="12" :xl="12">
+        <div class="titleBox flex">
+          <div><h2>请使用PC端体验</h2></div>
+          <div><a href="https://github.com/xiaodel/PersonalResume" target="_blank">开源地址</a></div>
+        </div>
+      </el-col>
+      <el-col :xs="24" :sm="24" :md="24" :lg="12" :xl="12">
+        <div class="nav">
+          <div class="item" v-for="(value,key) in templates" :key="key">
+            <RouterLink :to="`/details/${key}`" class="link">
+              <component :is="value" :data="data"></component>
+            </RouterLink>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <style lang="scss">
+.pages {
+  max-width: 1200px;
+  margin: auto;
 
-.nav {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-
-  .item {
-    width: 600px;
-    height: 800px;
-    overflow-y: auto;
-    border: 1px solid #2c3e50;
-    transform: scale(0.5);
-
-    &::-webkit-scrollbar {
-      width: 8px;
-      height: 10px;
-    }
-
+  .titleBox {
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
   }
-}
 
-@media (max-width: 600px) {
   .nav {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
     .item {
-      min-width: 600px;
+      width: 600px;
       height: 800px;
       overflow-y: auto;
       border: 1px solid #2c3e50;
       transform: scale(0.5);
+
+      &::-webkit-scrollbar {
+        width: 8px;
+        height: 10px;
+      }
+
+    }
+  }
+
+  @media (max-width: 600px) {
+    .nav {
+      .item {
+        min-width: 600px;
+        height: 800px;
+        overflow-y: auto;
+        border: 1px solid #2c3e50;
+        transform: scale(0.5);
+      }
     }
   }
 }
